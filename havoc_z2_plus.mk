a9f9d519d1741a6c2125b283208231fd57315e39
nikhilgohil <nikhil.gohil3@gmail.com>
2019-11-27 13:56:25 +0530

    Havoc Bringup

--- havoc_z2_plus.mk
+++ havoc_z2_plus.mk
@@ -20,13 +20,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from z2_plus device
 $(call inherit-product, device/zuk/z2_plus/device.mk)
 
-# Inherit some common PixelExperience stuff.
+# Inherit some common Havoc stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/havoc/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_z2_plus
+PRODUCT_NAME := havoc_z2_plus
 PRODUCT_DEVICE := z2_plus
 PRODUCT_BRAND := ZUK
 PRODUCT_MODEL := Z2 Plus
